23518f30f41029386206990a30d032beee9bd5b5
Ishak Leghlam <ishakleghlam@MacBook-Pro-de-Ishak.local>
2017-10-23 22:57:33 +0200

    create story

--- src/bot.js
+++ src/bot.js
@@ -4,11 +4,61 @@ const Fiat = require('./lib/Fiat')
 
 let bot = new Bot()
 
+let i = 0;
+
+const demo = [
+  {
+    name: 'ishak',
+    escrowBalance: 12.5,
+    rates: 74.5,
+    volumeTrade: 320,
+    currency: '$',
+    address: '33 rue la fayette, 75009 Paris France',
+    telegram: 'ishak',
+  },
+  {
+    name: 'ishak2',
+    escrowBalance: 12.5,
+    rates: 74.5,
+    volumeTrade: 320,
+    currency: '$',
+    address: '33 rue la fayette, 75009 Paris France',
+    telegram: 'ishak',
+  },
+  {
+    name: 'ishak3',
+    escrowBalance: 12.5,
+    rates: 74.5,
+    volumeTrade: 320,
+    currency: '$',
+    address: '33 rue la fayette, 75009 Paris France',
+    telegram: 'ishak',
+  }
+]
+
+const controls = [
+  {type: 'button', label: 'Next', value: 'next'},
+  {type: 'button', label: 'Contact', value: 'contact'},
+  {type: 'button', label: 'New address', value: 'new'},
+]
+
+const formatedTeller = data => `
+NAME: ${data.name}
+BALANCE: ${data.escrowBalance} ETH
+RATES: ${data.rates}
+VOLUME TRADE: ${data.volumeTrade} ETH
+CURRENCY: ${data.currency}
+ADDRESS:\n${data.address}
+`
+const formatedContact = data => `
+NAME: ${data.name}
+TELEGRAM: ${data.telegram}
+`
 // ROUTING
 
-bot.onEvent = function(session, message) {
+bot.onEvent = (session, message) => {
   switch (message.type) {
-    case "Init":
+    case 'Init':
       welcome(session)
       break
     case 'Message':
@@ -20,58 +70,79 @@ bot.onEvent = function(session, message) {
   }
 }
 
-function onMessage(session, message) {
-  console.log('sisi', message.content.body);
-  tellers(session)
-}
-
-function onCommand(session, command) {
+const onCommand = (session, command) => {
   switch (command.content.value) {
-    case 'tellers':
-      tellers(session)
-      break
-    case 'next':
-      next(session)
-      break
     case 'contact':
       contact(session)
-      break
-    }
+      break;
+    case 'next':
+      next(session)
+      break;
+    case 'new':
+      welcome(session)
+      break;
+  }
 }
+
 
 // STATES
+const welcome = session =>
+  session.reply(
+    SOFA.Message({
+      body: 'Hello from Dether! Please enter your address to locate the closest tellers',
+      showKeyboard: true
+    })
+  )
 
-function welcome(session) {
-  sendMessage(session, `Hello from Dether!`)
+const onMessage = (session, message) => {
+  setTimeout(() => {
+      // console.log('sisi', message.content.body);
+    // addrError(session);
+    teller(session, demo[i]);
+  }, 3000)
 }
 
-function tellers(session) {
-  // get from from addr
-  // how to get txt from user
-  sendMessage(session, 'Send teller here');
-}
+const addrError = session => session.reply(
+    SOFA.Message({
+      body: 'Invalid address, please enter your address',
+      showKeyboard: true
+    })
+  )
 
-function contact(session) {
-  // get telegram address
-  sendMessage(session, 'Send telegram address link');
-}
-
-function next(session) {
-  // next tellers
-  sendMessage(session, 'Next teller');
-}
-
-// HELPERS
-
-function sendMessage(session, message) {
-  let controls = [
-    {type: 'button', label: 'Get tellers', value: 'tellers'},
-    {type: 'button', label: 'Next', value: 'next'},
-    {type: 'button', label: 'Contact', value: 'contact'},
-  ]
-  session.reply(SOFA.Message({
-    body: message,
+const teller = (session, teller) => session.reply(
+  SOFA.Message({
+  	body: formatedTeller(teller),
+   	attachments: [{
+    	"type": "image",
+     	"url": "dether.png"
+    }],
     controls: controls,
     showKeyboard: false,
-  }))
+  })
+)
+
+const contact = session => session.reply(
+  SOFA.Message({
+  	body: formatedContact(demo[i]),
+   	attachments: [{
+    	"type": "image",
+     	"url": "dether.png"
+    }],
+    showKeyboard: false,
+  })
+)
+
+const next = session => {
+  i++;
+  session.reply(
+    SOFA.Message({
+    	body: formatedTeller(demo[i]),
+     	attachments: [{
+      	"type": "image",
+       	"url": "dether.png"
+      }],
+      controls: controls,
+      showKeyboard: false,
+    })
+  )
 }
