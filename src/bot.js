132f6cb972ef7c4bceb6ea2c3d3b6e2fe96113f4
Ishak Leghlam <ishakleghlam@MacBook-Pro-de-Ishak.local>
2017-10-24 11:28:49 +0200

    Formated data

--- src/bot.js
+++ src/bot.js
@@ -7,84 +7,17 @@ const Fiat = require('./lib/Fiat')
 
 let bot = new Bot()
 
+// DATA
 let i = 0;
 
 const provider = 'https://kovan.infura.io/v604Wu8pXGoPC41ARh0B';
+
 const gpscoord = {
   lng: 2.35237,
   lat: 48.88361,
 }; // france
 
-const getTeller = latlng => {
-  mapboxAPI.getcountrycode(latlng)
-    .then((countrycode) => {
-      console.log('countrycode -> ', countrycode)
-      detherGateway.default.tellers.getZone(countrycode, provider)
-        .then((tellers) => {
-          // Call teller
-          console.log(tellers);
-          return tellers;
-        })
-        .catch((e) => {
-          console.log(e);
-          return null;
-        })
-    })
-    .catch((e) => {
-      console.log(e);
-      return null;
-    })
-}
-
-// const getTellers = () => {
-//   getTeller(gpscoord)
-//     .then((tellers) => {
-//       return mapboxAPI.sortGpsCoord(gpscoord, tellers);
-//     })
-//     .then((sortTeller) => {
-//       demo = sortTeller;
-//     })
-//     .catch((e) => {
-//       console.log(e);
-//     })
-// };
-
-
-
-
-
-
-let demo = [];
-
-// [
-//   {
-//     name: 'ishak',
-//     escrowBalance: 12.5,
-//     rates: 74.5,
-//     volumeTrade: 320,
-//     currency: '$',
-//     address: '33 rue la fayette, 75009 Paris France',
-//     telegram: 'ishak',
-//   },
-//   {
-//     name: 'ishak2',
-//     escrowBalance: 12.5,
-//     rates: 74.5,
-//     volumeTrade: 320,
-//     currency: '$',
-//     address: '33 rue la fayette, 75009 Paris France',
-//     telegram: 'ishak',
-//   },
-//   {
-//     name: 'ishak3',
-//     escrowBalance: 12.5,
-//     rates: 74.5,
-//     volumeTrade: 320,
-//     currency: '$',
-//     address: '33 rue la fayette, 75009 Paris France',
-//     telegram: 'ishak',
-//   }
-// ]
+let allTellers = [];
 
 const controls = [
   {type: 'button', label: 'Next', value: 'next'},
@@ -105,12 +38,20 @@ VOLUME TRADE: ${data.volumeTrade} ETH
 CURRENCY: ${data.currency}
 ADDRESS:\n${data.address}
 `
+
 const formatedContact = data => `
 NAME: ${data.name}
 TELEGRAM: ${data.telegram}
 `
-// ROUTING
 
+
+
+
+
+
+
+
+// ROUTING
 bot.onEvent = (session, message) => {
   switch (message.type) {
     case 'Init':
@@ -140,6 +81,13 @@ const onCommand = (session, command) => {
 }
 
 
+
+
+
+
+
+
+
 // STATES
 const welcome = session =>
   session.reply(
@@ -149,32 +97,33 @@ const welcome = session =>
     })
   )
 
+const getTeller = latlng =>
+  new Promise((res, rej) => {
+    mapboxAPI.getcountrycode(latlng)
+      .then((countrycode) => detherGateway.default.tellers.getZone(countrycode, provider))
+      .then((tellers) => res(tellers))
+      .catch((e) => rej(e))
+  })
+
 const onMessage = (session, message) => {
   getTeller(gpscoord)
-    .then((tellers) => {
-      return mapboxAPI.sortGpsCoord(gpscoord, tellers);
-    })
+    .then(tellers => mapboxAPI.sortGpsCoord(gpscoord, tellers))
     .then((sortTeller) => {
-      demo = sortTeller;
-      console.log(sortTeller);
-      teller(session, demo[i]);
+      allTellers = sortTeller.slice(0, 10);
+      teller(session, allTellers[i]);
     })
-    .catch((e) => {
-      console.log(e);
-      addrError(session);
-    })
-
-  //
-  // setTimeout(() => {
-  //     // console.log('sisi', message.content.body);
-  //   // addrError(session);
-  //   teller(session, demo[i]);
-  // }, 3000)
+    .catch(e => addrError(session))
 }
 
+
+
+
+
+
+// FORMATED DATA
 const addrError = session => session.reply(
     SOFA.Message({
-      body: 'Invalid address, please enter your address',
+      body: 'Invalid address, please enter a valid address',
       showKeyboard: true
     })
   )
@@ -193,7 +142,7 @@ const teller = (session, teller) => session.reply(
 
 const contact = session => session.reply(
   SOFA.Message({
-  	body: formatedContact(demo[i]),
+  	body: formatedContact(allTellers[i]),
    	attachments: [{
     	"type": "image",
      	"url": "dether.png"
@@ -205,9 +154,10 @@ const contact = session => session.reply(
 
 const next = session => {
   i++;
+  if (i > 9) return welcome(session);
   session.reply(
     SOFA.Message({
-    	body: formatedTeller(demo[i]),
+    	body: formatedTeller(allTellers[i]),
      	attachments: [{
       	"type": "image",
        	"url": "dether.png"
